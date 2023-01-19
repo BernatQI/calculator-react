2ba9d5c16f228488176361ddcaf2600fef133fd3
BernatQi <bernatqipro@gmail.com>
2023-01-19 17:50:50 +0100

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
 import './App.css';
-import BernatQiLogo from './img/bernatqi-logo.png';
+import Button from './components/Button';
+import Result from './components/Result';
+import ButtonClear from './components/ButtonClear';
+import BernatQiLogo from './components/BernatQiLogo';
+import { useState } from 'react';
+import { evaluate } from 'mathjs';
 
 function App() {
+
+  const [input, setInput] = useState('');
+
+  const addInput = value => {
+    setInput(input + value);
+  };
+
+  const calculateResult = () => {
+    if (input) {
+      setInput(evaluate(input));
+    } else {
+      alert('Please type some value to calculate');
+    }
+  };
+
   return (
     <div className='App'>
-      <div className='bernatqi-logo-container'>
-        <img
-          src={BernatQiLogo}
-          className='bernatqi-logo'
-          alt='Logo BernatQi' />
-        <div className='calculator-container'>
-
+      <BernatQiLogo />
+      <div className='calculator-container'>
+        <Result input={input} />
+        <div className='row'>
+          <Button manageClick={addInput}>1</Button>
+          <Button manageClick={addInput}>2</Button>
+          <Button manageClick={addInput}>3</Button>
+          <Button manageClick={addInput}>+</Button>
+        </div>
+        <div className='row'>
+          <Button manageClick={addInput}>4</Button>
+          <Button manageClick={addInput}>5</Button>
+          <Button manageClick={addInput}>6</Button>
+          <Button manageClick={addInput}>-</Button>
+        </div>
+        <div className='row'>
+          <Button manageClick={addInput}>7</Button>
+          <Button manageClick={addInput}>8</Button>
+          <Button manageClick={addInput}>9</Button>
+          <Button manageClick={addInput}>*</Button>
+        </div>
+        <div className='row'>
+          <Button manageClick={calculateResult}>=</Button>
+          <Button manageClick={addInput}>0</Button>
+          <Button manageClick={addInput}>.</Button>
+          <Button manageClick={addInput}>/</Button>
+        </div>
+        <div className='row'>
+          <ButtonClear manageClear={() => setInput('')}>Clear</ButtonClear>
         </div>
       </div>
     </div>
---
+++ src/components/Result.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '../stylesheets/Result.css'
+
+const Result = ({ input }) => (
+  <div className="input">
+    {input}
+  </div>
+);
+
+export default Result;
